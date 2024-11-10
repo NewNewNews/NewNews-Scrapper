3b16eba5beac5dbb85d5ac6866da5f2d79065af9
Poonyawat Suntiniwat <ninekrub139@gmail.com>
2024-11-10 22:24:56 +0700

    congrat

--- main.py
+++ main.py
@@ -90,17 +90,14 @@ class NewsService(news_service_pb2_grpc.NewsServiceServicer):
 
     def UpdateNews(self, request, context):
         url = request.url
+        update_data = {}
 
-        current_news = self.collection.find_one({"url": url})
-
-        if current_news == None:
-            return news_service_pb2.UpdateNewsResponse(success=False, message = "Can't find news with URL")
-
-        update_data = {
-            "data": request.data if request.data != "" else current_news.data,
-            "category": request.category if request.category != "" else current_news.category, 
-            "date": request.date if request.date != "" else current_news.date
-        }
+        if request.data != "":
+            update_data["data"] = request.data
+        if request.category != "":
+            update_data["category"] = request.category
+        if request.date != "":
+            update_data["date"] = request.date
        
         result = self.collection.update_one(
             {"url": url},
